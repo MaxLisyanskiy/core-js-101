946c34e25b6aa9eb9fbef1cea242befff1bdcbf5
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-06-28 18:36:28 +0300

    feat: add solution for 6-task

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -5,7 +5,6 @@
  *                                                                                                *
  ************************************************************************************************ */
 
-
 /**
  * Return Promise object that is resolved with string value === 'Hooray!!! She said "Yes"!',
  * if boolean value === true is passed, resolved with string value === 'Oh no, she said "No".',
@@ -28,10 +27,16 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
-}
+function willYouMarryMe(answer) {
+  const SUCCESS = 'Hooray!!! She said "Yes"!';
+  const FAILED = 'Oh no, she said "No".';
+  const ERROR = 'Wrong parameter is passed! Ask her again.';
 
+  return new Promise((resolve, reject) => {
+    if (typeof answer !== 'boolean') reject(new Error(ERROR));
+    resolve(answer ? SUCCESS : FAILED);
+  });
+}
 
 /**
  * Return Promise object that should be resolved with array containing plain values.
@@ -48,8 +53,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array).then((values) => values);
 }
 
 /**
@@ -71,8 +76,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array).then((values) => values);
 }
 
 /**
